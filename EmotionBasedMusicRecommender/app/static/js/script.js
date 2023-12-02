0255c37f6565c0331ea3f93d7a2ec447a90e68fb
sanathbhimsen <sanathbhimsen26@gmail.com>
2023-12-02 02:19:32 -0500

    Connected the UI and Backend and able to fetch random songs at the moment, will work on the model next

--- EmotionBasedMusicRecommender/app/static/js/script.js
+++ EmotionBasedMusicRecommender/app/static/js/script.js
@@ -1,33 +1,35 @@
+// static/js/script.js
+
 function displaySpotifyTrack() {
     var emotionInput = document.getElementById('emotion-input');
     var spotifyIframeDiv = document.getElementById('spotify-iframe');
-    var recmessage = document.getElementById('recommendation-message');
+    var recommendationMessage = document.getElementById('recommendation-message');
 
     if (emotionInput.value.trim() === '') {
-        // Empty check
-        emotionInput.classList.add('empty-input'); 
+        emotionInput.classList.add('empty-input');
         emotionInput.style.borderColor = 'red';
         emotionInput.placeholder = 'This field cannot be empty. Tell us about your day :)';
-        recmessage.style.display = 'none';
+        recommendationMessage.style.display = 'none';
         spotifyIframeDiv.style.display = 'none';
-        return; 
+        return;
     } else {
-        emotionInput.classList.remove('empty-input'); 
+        fetch('/get_random_track')
+            .then(response => response.json())
+            .then(data => {
+                var trackId = data.track_id;
+                var iframeHtml = `<iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/${trackId}?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>`;
+                spotifyIframeDiv.innerHTML = iframeHtml;
+                spotifyIframeDiv.style.display = 'block';
+                spotifyIframeDiv.style.opacity = 1;
+                recommendationMessage.style.display = 'block';
+                recommendationMessage.style.opacity = 1;
+            })
+            .catch(error => console.error('Error:', error));
     }
-
-    var recommendationMessage = document.getElementById('recommendation-message');
-    recommendationMessage.style.display = 'block';
-    recommendationMessage.style.opacity = 1;
-
-    var iframeHtml = '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/3USxtqRwSYz57Ewm6wWRMp?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>';
-    spotifyIframeDiv.innerHTML = iframeHtml;
-    spotifyIframeDiv.style.display = 'block';
-    spotifyIframeDiv.style.opacity = 1;
 }
 
-
 document.getElementById('emotion-input').addEventListener('focus', function() {
-    this.classList.remove('empty-input'); 
-    this.style.borderColor = ''; 
-    this.placeholder = 'How are you feeling today?'; 
+    this.classList.remove('empty-input');
+    this.style.borderColor = '';
+    this.placeholder = 'How are you feeling today?';
 });
